414121075f4fe0cbf0a37e33ff44112bde1f1345
Bhanu Prabhat <bhanuprabhat95@gmail.com>
2026-03-26 18:31:53 +0530

    using jasmine testing framework on money.js file

---
+++ javascript-amazon-project-main/jasmine-standalone-5.1.1/moneyTest.js
@@ -0,0 +1,15 @@
+import { changeCurrencyFormat } from '../scripts/utils/money.js'
+
+describe('Test Suite: Formating currency', () => {
+    it('changing paisa to rupay', () => {
+        expect(changeCurrencyFormat(2095)).toEqual('20.95');
+    });
+
+    it('working with 0', () => {
+        expect(changeCurrencyFormat(0)).toEqual('0.00');
+    });
+
+    it('Round off the number', () => {
+        expect(changeCurrencyFormat(2000.4)).toEqual('20.00');
+    });
+});
