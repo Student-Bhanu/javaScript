f06550c314c2cbd676b461c0615cb6c73974f2f2
Bhanu Prabhat <bhanuprabhat95@gmail.com>
2026-03-29 14:50:03 +0530

    load products beforeAll hook

--- javascript-amazon-project-main/Tests/checkout/orderSummaryTest.js
+++ javascript-amazon-project-main/Tests/checkout/orderSummaryTest.js
@@ -1,10 +1,18 @@
 import { renderOrderSummaryPage } from '../../scripts/checkout/orderSummary.js'
 import { loadFromStorage, cart } from '../../data/cart.js'
+import { loadProductsFromBackend } from '../../data/products.js'
 
 describe('Test Suit: Render the order summary page', () => {
 
     const productId1 = 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6';
     const productId2 = '15b6fc6f-327a-4ec4-896f-486349e85a3d';
+
+    // run before all tests cases
+    beforeAll((done) => {
+        loadProductsFromBackend(() => {
+            done(); // after completion of this function all the remaining tests will run
+        });
+    })
 
     // hooks is the code which is sharable between tests
     // beforeEach runs before every tests
